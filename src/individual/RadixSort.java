77f197966f7b218de017af218e70089d86e71caa
Henry Dario Moreno Espitia <hmoreno@co0c02fd683md6m.ml.com>
2021-05-26 16:42:14 -0500

    RadixSort:: ading package called individual

---
+++ src/individual/RadixSort.java
@@ -0,0 +1,26 @@
+package individual;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RadixSort {
+
+    public static void main(String[] args) {
+	// write your code here
+        // define array to sort
+
+        List<Integer> listToSort = new ArrayList<>();
+        listToSort.add(32);
+        listToSort.add(132);
+        listToSort.add(102);
+        listToSort.add(4);
+
+        // convert arraylist`s numbers to strings
+        // adding zeros to complete size of each string
+
+        // create list to keep 0 to 9 strings
+
+        // go through each element in the arrayToSort and copy in the list accordingly to "n" digit
+        
+    }
+}
