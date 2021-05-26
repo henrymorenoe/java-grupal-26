d4320d93b707d1281b6fa6600b2657f56c312c73
Henry Dario Moreno Espitia <hmoreno@co0c02fd683md6m.ml.com>
2021-05-26 17:39:32 -0500

    RadixSort:: adding zeros

--- src/individual/RadixSort.java
+++ src/individual/RadixSort.java
@@ -21,13 +21,27 @@ public class RadixSort {
         // convert arraylist`s numbers to strings
             // go through each element.
             String stringNumber  = "";
+            int mostLength = 0;
+            int elementLength = 0;
             for(Integer element: listToSort){
                 //System.out.println("Element: " + element)
                 stringNumber = element.toString();
-                stringNumber = addZeros(stringNumber);
                 listToSortString.add(stringNumber);
+                // find out the largest digit amount
+                elementLength = stringNumber.length();
+                if(elementLength > mostLength){
+                    mostLength = elementLength;
+                }
             }
-
+            List<String> listToSort_zeros = new ArrayList<>();
+            // adding zeros:
+            String theStringZeros = new String();
+            for(String element: listToSortString){
+                //System.out.println("Element: " + element);
+                theStringZeros = addZeros(element, mostLength);
+                listToSort_zeros.add( theStringZeros);
+                System.out.println("Element: " + theStringZeros);
+            }
 
 
         // create list to keep 0 to 9 strings
@@ -39,16 +53,18 @@ public class RadixSort {
         // get and put each element from the digit lists into arrayToSorter
 
 
-
-
     }
 
     private static String addZeros(String stringNumber, int desiredSize) {
-        String theStringNumber_ = new String(stringNumber);
+        StringBuilder theStringNumber_ = new StringBuilder(stringNumber);
+        //System.out.println("antes: "+ theStringNumber_);
         Integer zerosToAdd = desiredSize - theStringNumber_.length();
+        //System.out.println("cantidad de zeros"+ zerosToAdd);
         for(int i = 0; i<zerosToAdd; i++){
-            theStringNumber_
+            theStringNumber_.insert(0,"0");
         }
+        //System.out.println("despues: "+ theStringNumber_);
+        return theStringNumber_.toString() ;
 
     }
 }
