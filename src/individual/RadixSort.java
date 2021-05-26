4d45aec371cc721f5bee3c7a03cc67d5856d7319
Henry Dario Moreno Espitia <hmoreno@co0c02fd683md6m.ml.com>
2021-05-26 17:09:26 -0500

    RadixSort class: converting numbers to String

--- src/individual/RadixSort.java
+++ src/individual/RadixSort.java
@@ -2,6 +2,7 @@ package individual;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class RadixSort {
 
@@ -15,12 +16,39 @@ public class RadixSort {
         listToSort.add(102);
         listToSort.add(4);
 
+        List<String> listToSortString = new ArrayList<>();
+
         // convert arraylist`s numbers to strings
-        // adding zeros to complete size of each string
+            // go through each element.
+            String stringNumber  = "";
+            for(Integer element: listToSort){
+                //System.out.println("Element: " + element)
+                stringNumber = element.toString();
+                stringNumber = addZeros(stringNumber);
+                listToSortString.add(stringNumber);
+            }
+
+
 
         // create list to keep 0 to 9 strings
 
         // go through each element in the arrayToSort and copy in the list accordingly to "n" digit
-        
+
+        // clean arrayToSorted
+
+        // get and put each element from the digit lists into arrayToSorter
+
+
+
+
+    }
+
+    private static String addZeros(String stringNumber, int desiredSize) {
+        String theStringNumber_ = new String(stringNumber);
+        Integer zerosToAdd = desiredSize - theStringNumber_.length();
+        for(int i = 0; i<zerosToAdd; i++){
+            theStringNumber_
+        }
+
     }
 }
